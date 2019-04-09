a8283ccf504b0616cf958e2cabebe8b4bdad0567
Jan-Christian Benk <Jacdesigns123@gmail.com>
2019-04-09 11:49:14 +0200

    Update for cameras

--- src/Camera.h
+++ src/Camera.h
@@ -117,9 +117,5 @@ public:
 	/*!
 	* move camera
 	*/
-	void Camera::positionUpdate(glm::vec3 newPosition) {
-		_position += newPosition;
-		_viewMatrix = glm::lookAt(_position, _front+_position, glm::vec3(0.0, 1.0, 0.0));
-
-	}
+	void positionUpdate(glm::vec3 newPosition);
 };
